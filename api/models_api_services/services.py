4d47ff65d257b91f8f3276e70ad263c02c56a201
amare1990 <amaremek@gmail.com>
2025-01-13 16:43:59 +0300

    Update the class model loading and prediction method

--- api/models_api_services/services.py
+++ api/models_api_services/services.py
@@ -6,7 +6,18 @@ import numpy as np
 
 class PredictionService:
   def __init__(self, model_path='../../data/model.pkl'):
-    self.model = pickle.load(model_path)
+    # self.model = pickle.load(model_path)
+    self.model = self.load_model(model_path)
+
+
+  def load_model(model_path):
+    try:
+      with open(model_path, 'rb') as file:
+        model = pickle.load(file)
+      return model
+    except Exception as e:
+      raise ValueError(f"Error loading model: {e}")
+
 
 
 def predict(self, data):
